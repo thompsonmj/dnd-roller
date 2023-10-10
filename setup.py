fa0bab26b75572e637a54828d84318f64c12a9c0
Matt Thompson <thompson.4509@osu.edu>
2023-10-10 14:18:53 -0400

    dnd-roller 0.0.1

---
+++ setup.py
@@ -0,0 +1,25 @@
+# file: setup.py
+
+import os
+from pathlib import Path
+from setuptools import find_packages, setup
+
+PKG_FOLDER = Path(os.path.abspath(os.path.dirname(__file__)))
+
+with open(PKG_FOLDER / "README.md") as f:
+    long_description = f.read()
+
+
+setup(
+    name="dnd-roller",
+    version="0.0.1",
+    author="Matt Thompson",
+    author_email="thompson.4509@osu.edu",
+    description="Python package to roll D&D dice in the terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/thompsonmj/dnd-roller",
+    include_package_data=True,
+    packages=find_packages(exclude=[]),
+    install_requires=["tabulate"],
+)
